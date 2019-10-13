2c29f7cba3388667765d9cbc7d7cc95ee93b5ad0
j3s1 <scaramuuza@gmail.com>
2019-10-13 16:44:56 -0300

    serealizer respuesta

--- encuesta/serializers.py
+++ encuesta/serializers.py
@@ -13,24 +13,19 @@ class OrganizacionesSerializer(serializers.ModelSerializer):
         fields = ('nombre','id','uuid', 'cantidad_empleado')
 
 class RespuestasSerializer(serializers.ModelSerializer):
-    preguntaR = serializers.PrimaryKeyRelatedField(read_only=True)
-    preguntas = PreguntasSerializer(many=True,read_only = True)
-    organizacionR = serializers.PrimaryKeyRelatedField(read_only=True)
-    organizaciones = OrganizacionesSerializer(many=False,read_only = True)
+    organizacion_uuid = serializers.CharField(write_only=True)
     class Meta:
         model = Respuestas
-        fields = ('descripcion', 'id', 'preguntaR','preguntas', 'organizacionR', 'organizaciones')
+        fields = ('descripcion', 'id','organizacion_uuid','pregunta')
+    def create(self, validated_data):
+        org = Organizaciones.objects.get(uuid=validated_data['organizacion_uuid'])
+        print (validated_data['pregunta'] )
+        res = Respuestas.objects.create(
+            descripcion =  validated_data['descripcion'],
+            pregunta = validated_data['pregunta'],
+            organizacion= org, 
+           
+        )
+        return res
 
-    #def create(self, validated_data):
-     #   return Respuestas(**validated_data)
 
-"""class OrganizacionSerializer(serializers.ModelSerializer):
-    # uuid = serializers.CharField(read_only=True)
-
-    class Meta:
-        model = Organizaciones
-        fields = ('id', 'nombre', 'uuid')
-        extra_kwargs = {
-            'uuid': {'read_only': True}
-        }
- """
--- encuesta/views.py
+++ encuesta/views.py
@@ -18,16 +18,6 @@ class PreguntasViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['tipo_Usuario',]
     
- #   @link()
-    #def list_preg_por_tipo(self, request, *args, **kwargs):
-     #   tipo_user = self.get_object()
-      #  preg_query = Preguntas.objects.filter(activo=True, tipo_Usuario=tipo_user)
-       # schedule_json = PreguntasSerializer(preg_query)
-        #if serializer_class.is_valid():
-         #   serializer_class.validated_data
-        #return Response(serializer_class.data)
-
-
 class RespuestasViewSet(viewsets.ModelViewSet):
     queryset = Respuestas.objects.all()
     serializer_class = RespuestasSerializer
